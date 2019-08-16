5ca80b9c892dee7c233919c1a2acf89ea025470e
Ian Hunter <ijh.hunter@gmail.com>
2019-08-15 23:11:13 -0400

    parse private key correctly through env variable

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ const admin = require('firebase-admin')
 const axios = require('axios')
 axios.defaults.headers.common['Client-ID'] = process.env.CLIENT_ID
 
-const serviceAccount = require('../jerma-joke-firebase-adminsdk-4vksc-6b4b6c4893.json')
+// const serviceAccount = require('../jerma-joke-firebase-adminsdk-4vksc-6b4b6c4893.json')
+
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
