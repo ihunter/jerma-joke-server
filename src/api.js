8c313b2f7405c353d693557f553200eab44e714c
ihunter <ijh.hunter@gmail.com>
2020-12-03 22:32:23 -0500

    humanize token experation with moment

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const axios = require('axios')
+const moment = require('moment')
 
 module.exports = () => {
   const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } = process.env
@@ -18,10 +19,10 @@ module.exports = () => {
   return async () => {
     const currentDate = Date.now()
     const durationInSeconds = Math.ceil((currentDate - startDate) / 1000)
-    console.log(`Token Expires In ${tokenExpiresIn - durationInSeconds} Seconds`)
+    console.log(`Token expires in ${moment.duration(tokenExpiresIn - durationInSeconds, 'seconds').humanize()}`)
 
     if ((tokenExpiresIn - durationInSeconds) <= 0) {
-      console.log('Token Expired, Requesting New Token')
+      console.log('Token expired, requesting new token')
       const res = await twitchAPI.post(
         `https://id.twitch.tv/oauth2/token?client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`
       )
@@ -29,7 +30,7 @@ module.exports = () => {
       token = res.data.access_token
       tokenExpiresIn = res.data.expires_in
       startDate = Date.now()
-      console.log(`New Token Expires In ${tokenExpiresIn} Seconds`)
+      console.log(`New token expires in ${moment.duration(tokenExpiresIn, 'seconds').humanize()}`)
     }
 
     twitchAPI.defaults.headers.common.Authorization = `Bearer ${token}`
