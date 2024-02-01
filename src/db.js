00430f28918aa4ae79eb4bebbd3cd49c98e3eb23
ihunter <ijh.hunter@gmail.com>
2024-01-31 23:35:30 -0500

    update db creds

--- src/db.js
+++ src/db.js
@@ -1,25 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const admin = require('firebase-admin')
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = {
-  type: process.env.TYPE,
-  project_id: process.env.PROJECT_ID,
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.CLIENT_EMAIL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: process.env.AUTH_URI,
-  token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.CLIENT_CERT_URL
-}
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://jerma-joke.firebaseio.com'
-})
+const app = initializeApp({
+  credential: cert(serviceAccount),
+  databaseURL: "https://jerma-joke.firebaseio.com",
+});
+
+const db = getFirestore(app);
 
 module.exports = {
-  db: admin.firestore()
-}
+  db: db,
+};
